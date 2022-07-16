be80488ab7ce960a10cc4a011e0748e4dd7bf060
king <emmanueltakor4@gmail.com>
2022-07-16 05:33:53 -0400

    commits

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-"use strict"
+// "use strict"
 
 
 class Person {
-    name;
+    name= "Takorr"
     address;
     race;
 
@@ -16,9 +16,12 @@ this.race = race
         return `Hello my name is ${this.name}.I am from ${this.address}.I am of ${this.race} descent.`
     }
 }
+Person.prototype.ageGroup ="Adult";
+Person.prototype.gender = "male";
 
 let personOne = new Person("Takor","Tiko","African");
-console.log(personOne.identity())
+
+console.log(personOne.gender)
 
 class Car {
   name;
@@ -29,10 +32,19 @@ class Car {
     this.capacity = capacity;
   }
 
+  get nameCar(){
+    return this.name.toUpperCase()
+}
+
   fullInfo() {
     return `My car is a ${this.name} and it has ${this.capacity}.`;
   }
 }
+let Car2= new Car("Honda","4000Powers")
+console.log(Car2.nameCar)
 
 let Car1 = new Car("BMW", "10,000Engine Capacity");
+Car.prototype.color ="red"
 console.log(Car1.fullInfo());
+console.log(Person.name)
+console.log(Car1.color)
