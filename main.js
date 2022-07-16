9e41444f3d02ba350171662de0b6692eb04e1474
king <emmanueltakor4@gmail.com>
2022-07-16 04:31:06 -0400

    commits

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
+"use strict"
+
+
 class Person {
     name;
     address;
     race;
-
 
     constructor(name,address,race){
 this.name = name
@@ -17,3 +19,20 @@ this.race = race
 
 let personOne = new Person("Takor","Tiko","African");
 console.log(personOne.identity())
+
+class Car {
+  name;
+  capacity;
+
+  constructor(name, capacity) {
+    this.name = name;
+    this.capacity = capacity;
+  }
+
+  fullInfo() {
+    return `My car is a ${this.name} and it has ${this.capacity}.`;
+  }
+}
+
+let Car1 = new Car("BMW", "10,000Engine Capacity");
+console.log(Car1.fullInfo());
